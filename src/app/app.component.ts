8061caacea9c8fbbfa916f9f2825b620e4206323
jeissonbarriga <jeissonbarrigadev@gmail.com>
2019-09-29 23:14:35 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { DoubleLinkedList } from './data-structures/double-linked-list';
+import { Product } from './data-structures/product';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private list: DoubleLinkedList<Product>){}
+
+  pushNodesFront(quantity: number){
+
+    let t0 = performance.now();
+    for(let i=0; i < quantity; i++){
+      let product = new Product(i, "Product"+i);
+      this.list.pushFront(product);
+    }
+    let t1 = performance.now();
+
+    //this.list.displayList();
+
+    console.log("push front "+ quantity +" nodes took: ");
+    console.warn((t1 - t0) + " milliseconds");
+  }
+  
+  pushNodesBack(quantity: number){
+
+    let t0 = performance.now();
+    for(let i=0; i < quantity; i++){
+      let product = new Product(i, "Product"+i);
+      this.list.pushBack(product);
+    }
+    let t1 = performance.now();
+
+    console.log("push back"+ quantity +" node took: ");
+    console.warn((t1 - t0) + " milliseconds");
+  }
+
+  findByKey() {
+    let t0 = performance.now();
+
+    let product = new Product(1, "Product"+1);
+    this.list.findByKey(product);
+    
+    let t1 = performance.now();
+
+    console.log("find 1 node took: ");
+    console.warn((t1 - t0) + " milliseconds");
+  }
+
+  popBack() {
+    let t0 = performance.now();
+    
+    this.list.popBack();
+    
+    let t1 = performance.now();
+
+    console.log("pop back 1 node took: ");
+    console.warn((t1 - t0) + " milliseconds");
+  }
+
+  resetList(){
+    this.list.clearList();
+  }
+
+}
