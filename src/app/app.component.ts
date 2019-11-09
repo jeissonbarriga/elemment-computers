387126922baee70516b3590299bfd2756975d1da
EdisonHuerfano <ecamilo10hue@gmail.com>
2019-11-08 23:56:51 -0500

    Added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DoubleLinkedList } from './data-structures/double-linked-list';
-import { DynamicArray } from './data-structures/dynamic-array';
 import { Product } from './data-structures/product';
 import { DynamicArray } from './data-structures/dynamic-array';
 
@@ -10,79 +9,12 @@ import { DynamicArray } from './data-structures/dynamic-array';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
-export class AppComponent {
-  posicion: number = 9000;
-
-  constructor(private list: DoubleLinkedList<Product>, private array: DynamicArray<Product>){}
-
-  //Dynamic Array:
-
-  getValue(index: number){
-    let t0 = performance.now();
-
-    let value = this.array.get(index);
-    console.log(value);
-
-    let t1 = performance.now();
-
-    console.log("get value at index "+ index +" took:")
-    console.warn((t1 - t0) + " milliseconds");
-  }
-
-  setValue(index: number, value: Product){
-    let t0 = performance.now();
-
-    this.array.set(index, value);
-
-    let t1 = performance.now();
-
-    console.log("set value "+ value +" at index "+ index +" took:")
-    console.warn((t1 - t0) + " milliseconds");
-  }
-  
-  pushValuesBack(quantity: number){
-
-    let t0 = performance.now();
-    for(let i=0; i < quantity; i++){
-      let product = new Product(i, "Product"+i);
-      this.array.pushBack(product);
-    }
-    let t1 = performance.now();
-    
-    this.array.displayArray();
-
-    console.log("push back "+ quantity +" value took: ");
-    console.warn((t1 - t0) + " milliseconds");
-  }
-
-  removeValue(index: number) {
-    let t0 = performance.now();
-
-    let eliminado = this.array.remove(this.posicion);
-    if (eliminado) {
-      console.log("Eliminado: " + eliminado.name);
-    }
-    
-    let t1 = performance.now();
-
-    console.log("remove 1 value took: ");
-    console.warn((t1 - t0) + " milliseconds");
-  }
-
-  display(){
-    this.array.displayArray();
-  }
-
-  //Doubly-linked List:
-=======
 export class AppComponent implements OnInit {
   posicion: number = 9000;
 
   ngOnInit() {
 
   }
->>>>>>> Lizeth
 
 
   constructor(private list: DoubleLinkedList<Product>, private dArray: DynamicArray<Product>) {
--- src/app/data-structures/dynamic-array.ts
+++ src/app/data-structures/dynamic-array.ts
@@ -1,79 +1,6 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-<<<<<<< HEAD
-    providedIn: 'root',
-})
-export class DynamicArray<T> {
-
-    array: Array<T>;
-    capacity: number;
-    size: number;   
-
-    constructor(){
-        this.array = new Array<T>(1);
-        this.capacity = 1;
-        this.size = 0;
-    }
-
-    get(index: number) : T {
-        if(index < 0 || index >= this.size) {
-            console.error("Error!!. Index out of range");
-        } else {
-            return this.array[index];
-        }
-    }
-
-    set(index: number, val : T) {
-        if(index >= this.size) {
-            console.error("Error!!. Index out of range");
-        } else {
-            this.array[index] = val;
-        }
-    }
-
-    pushBack(val: T) {
-        if(this.size == this.capacity) {
-            let new_array = new Array(this.capacity * 2);//arreglo con el doble de capacidad
-            for(let i = 0; i < this.size; i++) {
-                new_array[i] = this.array[i];
-            }
-            this.array = new_array;
-            this.capacity *= 2;
-        }
-        this.array[this.size] = val;
-        this.size++;
-    }
-
-    remove(index: number) : T {
-        if(index < 0 || index >= this.size){ 
-            console.error("Error!!. Index out of range");
-            return null;
-        } else {
-            let val = this.array[index];
-            for(let i = index; i < this.size-1; i++) {
-                this.array[i] = this.array[i+1];
-            }
-            this.size--;
-            return val;
-        }
-    }
-
-    getSize(){
-        return this.size;
-    }
-
-    displayArray(){
-        if(this.size == 0){
-            console.log("Empty list");
-        } else {
-            for(let i = 0; i < this.size; i++){
-                console.log(this.array[i]);
-            }
-        }
-    }
-
-=======
   providedIn: 'root',
 })
 export class DynamicArray<T> {
@@ -146,6 +73,5 @@ export class DynamicArray<T> {
       console.log(this.arr[i]);
     }
   }
->>>>>>> Lizeth
 
 }
